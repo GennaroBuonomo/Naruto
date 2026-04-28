7ae67655f005eb741aadde3bc87e5a3e89859c7a
GennaroBuonomo <gennarobuonomo66@gmail.com>
2026-04-28 19:36:26 +0200

    fix: correct jutsu names for ninja characters in the list

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import './App.css';
 
 
 const listaNinja = [
-  { id: 1, nome: "Naruto Uzumaki", tecnica: "Rasengan", villaggio: "Foglia", img: "/images/image 1 .jpg" },
-  { id: 2, nome: "Sasuke Uchiha", tecnica: "Chidori", villaggio: "Foglia", img: "/images/image 2 .jpg" },
+  { id: 1, nome: "Naruto Uzumaki", tecnica: "Rasen Shuriken", villaggio: "Foglia", img: "/images/image 1 .jpg" },
+  { id: 2, nome: "Sasuke Uchiha", tecnica: "Sharingan", villaggio: "Foglia", img: "/images/image 2 .jpg" },
   { id: 3, nome: "Sakura Haruno", tecnica: "Impatto del Fiore", villaggio: "Foglia", img: "/images/image 3 .jpg" },
-  { id: 4, nome: "Kakashi Hatake", tecnica: "Sharingan", villaggio: "Foglia", img: "/images/image 4 .jpg" },
-  { id: 5, nome: "Gaara", tecnica: "Scudo di Sabbia", villaggio: "Sabbia", img: "/images/image 5 .jpg" },
+  { id: 4, nome: "Kakashi Hatake", tecnica: "Chidori", villaggio: "Foglia", img: "/images/image 4 .jpg" },
+  { id: 5, nome: "Gaara", tecnica: "Sabaku Soso", villaggio: "Sabbia", img: "/images/image 5 .jpg" },
   { id: 6, nome: "Itachi Uchiha", tecnica: "Amaterasu", villaggio: "Alba", img: "/images/image 6 .jpg" },
-  { id: 7, nome: "Hinata Hyuga", tecnica: "Palmo d'Aria", villaggio: "Foglia", img: "/images/image 7 .jpg" },
-  { id: 8, nome: "Shikamaru Nara", tecnica: "Ombra", villaggio: "Foglia", img: "/images/image 8 .jpg" },
-  { id: 9, nome: "Jiraiya", tecnica: "Richiamo", villaggio: "Foglia", img: "/images/image 9 .jpg" },
-  { id: 10, nome: "Tsunade", tecnica: "Forza Bruta", villaggio: "Foglia", img: "/images/image 10 .jpg" },
+  { id: 7, nome: "Hinata Hyuga", tecnica: "Hakke Kusho", villaggio: "Foglia", img: "/images/image 7 .jpg" },
+  { id: 8, nome: "Shikamaru Nara", tecnica: "Kagemane", villaggio: "Foglia", img: "/images/image 8 .jpg" },
+  { id: 9, nome: "Jiraiya", tecnica: "Rasengan", villaggio: "Foglia", img: "/images/image 9 .jpg" },
+  { id: 10, nome: "Tsunade", tecnica: "Sozo Saisei", villaggio: "Foglia", img: "/images/image 10 .jpg" },
   { id: 11, nome: "Madara Uchiha", tecnica: "Hengoku", villaggio: "Foglia", img: "/images/image 11 .jpg" },
   { id: 12, nome: "Pain", tecnica: "Shinra Tensei", villaggio: "Alba", img: "/images/image 12 .jpg" }
 ];
